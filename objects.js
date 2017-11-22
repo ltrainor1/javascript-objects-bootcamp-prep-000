42808417d0abc79a9b402668f0105d1b33feb354
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-22 07:28:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle;
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName){
